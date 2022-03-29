4b04f01f316793e6fdb3d4cf13be3e44ba7543a7
jake25fuller <jake25fuller@gmail.com>
2022-03-29 15:18:57 -0400

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # CSV-Combiner
+# Written by Jake Fulller for the purpose of the PMG Graduate Leadership Program Interview
+# 03/29/22
+
+To use this shell script, use command "python csvc.py x.csv y.csv etc.csv > output.csv", where any amount of csv files can be used for inputs and the output file name can be whatever the user desires. The given fixtures csv files have been included for testing.
--- csvc.py
+++ csvc.py
@@ -1,2 +1,23 @@
 # Written by Jake Fulller for the purpose of the PMG Graduate Leadership Program Interview
 # 03/29/22
+
+import csv
+import sys
+
+# writer represents output file
+writer = csv.writer(sys.stdout)
+titles = True
+
+# for every input csv file
+for file in sys.argv[1:]:
+    # open the file in readable format
+    with open(file, 'r') as content:
+        reader = csv.reader(content)
+        # write each row to the output file
+        for i, row in enumerate(reader):
+            # check to make sure the column titles are written only once
+            if i == 0 and not titles:
+                continue
+            writer.writerow(row)
+    # title has been written
+    titles = False
